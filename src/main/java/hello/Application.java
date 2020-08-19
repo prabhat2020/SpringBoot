cac61f213e2baae80b834c305fbce08c8fa91f34
prabhat2020 <65116422+prabhat2020@users.noreply.github.com>
2020-08-19 12:15:03 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello Prabhat! This is hack";
+    return "Hello Prabhat! This is hack4";
   }
 
   public static void main(String[] args) {
